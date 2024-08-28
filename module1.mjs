52432ec18834484c1a83a2e7509a52a6601ace41
ChaudharyAbdullah06 <oye.abdullah04@gmail.com>
2024-08-28 15:37:15 +0500

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import ui,{c,d,b} from './module2.mjs'
+console.log(ui);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a="Abdullah";
+const b="Abubakar";
+const c="Zaki";
+const d="Aashir";
+
+export default a;
+export {b};
+export {c};
+export {d};
